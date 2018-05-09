a12c977bccafd17fdd62892a676ccab65e8b59f7
lw <hugo.liu@shangxue.la>
2018-05-09 17:26:53 +0800

    切面

---
+++ src/Ascpet.java
@@ -0,0 +1,9 @@
+public class Ascpet {
+    public void before() {
+        System.out.print("ascpet before");
+    }
+
+    public void after() {
+        System.out.print("ascpet after");
+    }
+}
